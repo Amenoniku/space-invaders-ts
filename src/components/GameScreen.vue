2b61a3cf777324d2012c2ecc7c5bd5ce6f67818f
Ame <okiniku@yahoo.com>
2021-11-28 13:22:57 +0300

    init

---
+++ src/components/GameScreen.vue
@@ -0,0 +1,21 @@
+<template>
+
+<canvas
+  class="game-screen"
+  ref="GameScreen"
+  width="400"
+  height="480"
+/>
+
+</template>
+
+<style lang='scss' scoped>
+
+.game-screen {
+  width: 400px;
+  height: 480px;
+  margin: 0 auto ;
+	border: 1px solid #B50000;
+}
+
+</style>
